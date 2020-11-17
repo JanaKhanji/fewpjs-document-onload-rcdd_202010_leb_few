9d8a90888a46f82929496a37482ba2f4c8132b2f
Jana Khanji <jana.khanji@ieee.org>
2020-11-17 07:22:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('#text').textContent="";
+  document.querySelector('#text').textContent="This is really
+cool!";
 });
